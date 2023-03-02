f9b5f1c72ece98cee6466ca6e3e36439bfafb546
kjin <pkjin.eng@gmail.com>
2023-03-02 17:49:38 +0900

    cors

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 const app = express();
 const port = 4000;
 
 require("dotenv").config();
 const { API_KEY } = process.env;
+
+app.use(cors());
 
 const API_URL = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/ListExhibitionOfSeoulMOAInfo/1/50/`;
 
